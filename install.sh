49a4a79a76580ddb5d834c08f2a392e3409c3961
scaik <67639168+scaik@users.noreply.github.com>
2024-11-29 15:02:22 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -223,8 +223,6 @@ execute_script "hyprland.sh"
 # Install AGS from source (older version)
 execute_script "ags.sh"
 
-execute_script "nodejs.sh"
-
 if [ "$nvidia" == "Y" ]; then
     execute_script "nvidia.sh"
 fi
