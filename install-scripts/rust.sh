17f0ef9b6a4f5ab92a7e7d57705683cfc281fedc
scaik <67639168+scaik@users.noreply.github.com>
2024-12-01 20:26:09 +0300

    Create rust.sh

---
+++ install-scripts/rust.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# 💫 https://github.com/JaKooLit 💫 #
+# Rust #
+if [[ $USE_PRESET = [Yy] ]]; then
+  source ./preset.sh
+fi
+
+rust=(
+  rustup
+)
+
+## WARNING: DO NOT EDIT BEYOND THIS LINE IF YOU DON'T KNOW WHAT YOU ARE DOING! ##
+
+# Determine the directory where the script is located
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Change the working directory to the parent directory of the script
+PARENT_DIR="$SCRIPT_DIR/.."
+cd "$PARENT_DIR" || exit 1
+
+source "$(dirname "$(readlink -f "$0")")/Global_functions.sh"
+
+# Set the name of the log file to include the current date and time
+LOG="Install-Logs/install-$(date +%d-%H%M%S)_rust.log"
+
+# Rust
+printf "${NOTE} Installing Rustup and Cargo binaries...\n"  
+  for RUST in "${rust[@]}"; do
+    install_package "$RUST" 2>&1 | tee -a "$LOG"
+    [ $? -ne 0 ] && { echo -e "\e[1A\e[K${ERROR} - $RUST Package installation failed, Please check the installation logs"; exit 1; }
+  done
+
+printf "\n%.0s" {1..2}
+
+cargo install cargo-update
+
+clear
